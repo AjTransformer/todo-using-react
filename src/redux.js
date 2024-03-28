a8fdcaed2addf46fb263f553d27cb6bcbad530c9
AJ <akjwhatsapps@gmail.com>
2024-03-28 22:49:35 +0530

    complete and delete functionality added

--- src/redux.js
+++ src/redux.js
@@ -11,10 +11,23 @@ export function addTask(myTask){
     }
 }
 
+export function removeTask(myTask){
+    return{
+        type:"Remove_Task",
+        payload:myTask
+    }
+}
+
 export function Myreducer(info=state , action){
     if(action.type === "Add_Task"){
         return{
             tasks:[...info.tasks , action.payload]
+        }
+    }
+    if(action.type === "Remove_Task"){
+        const upDatedArray= info.tasks.filter(task =>task!==action.payload)
+        return{
+            tasks:upDatedArray
         }
     }
     return info;
