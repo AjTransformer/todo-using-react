d4a59112706fb19892235d09edf433ee1bd56316
AJ <akjwhatsapps@gmail.com>
2024-03-30 00:01:18 +0530

    Edit Functionality Added

--- src/App.js
+++ src/App.js
@@ -2,29 +2,50 @@ import './App.css';
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import A from './A';
-import { addTask } from './redux';
+import { addTask,editTask} from './redux';
 
 function App() {
   const [task, setTask] = useState("");
+  const [editedValue , setEditValue] = useState(null)
   const todoDispatcher = useDispatch();
+  // const [edit,setEdit] = useState("")
 
-  function addTheTask() {
-    todoDispatcher(addTask(task));
+  // function addTheTask() {
+  //   todoDispatcher(addTask(task));
+  //   setTask("");
+  // }
+
+  function addOrEditTask() {
+    if (editedValue !== null) {
+      // If editIndex is not null, it means we're editing an existing task
+      // Update the task at the editIndex
+      // Here, we're assuming you have an action to update a task
+      // todoDispatcher(updateTask(editIndex, task));
+      // Reset editIndex
+      setEditValue(null);
+      todoDispatcher(editTask(task))
+    } else {
+      // If editIndex is null, it means we're adding a new task
+      todoDispatcher(addTask(task));
+    }
+    // Reset the task input field
     setTask("");
   }
 
   return (
     <div className="container">
       <div className="input-container">
-        <span>Enter The Task Name:</span>
+        <span>{editedValue !== null ? "Edit" : "Enter"} The Task Name:</span>
         <input 
           value={task} 
-          onChange={e => setTask(e.target.value)}
+          onChange={e => {setTask(e.target.value)}}
           placeholder="Enter task name"
         />
-        <button onClick={addTheTask}>Add Task</button>
+        <button onClick={function(){
+          addOrEditTask()
+          }}>{editedValue==null? "Add Task":"Edit Task"}</button>
       </div>
-      <A />
+      <A setEditValue={setEditValue} setTask={setTask}/>
     </div>
   );
 }
--- src/redux.js
+++ src/redux.js
@@ -4,6 +4,8 @@ const state = {
     tasks:[]
 }
 
+let lastAddedTask = "";
+
 export function addTask(myTask){
     return{
         type:"Add_Task",
@@ -14,6 +16,20 @@ export function addTask(myTask){
 export function removeTask(myTask){
     return{
         type:"Remove_Task",
+        payload:myTask
+    }
+}
+
+export function editTask(myTask){
+    return{
+        type:"Edit_Task",
+        payload:myTask
+    }
+}
+
+export function lastTask(myTask){
+    return{
+        type:"Last_Task",
         payload:myTask
     }
 }
@@ -30,6 +46,23 @@ export function Myreducer(info=state , action){
             tasks:upDatedArray
         }
     }
+
+    if(action.type==="Edit_Task"){
+        const editedTask = info.tasks.map(task=> {
+            if(task==lastAddedTask){
+                return action.payload
+            }
+            return task
+        });
+        return{
+            tasks:editedTask
+        }
+    }
+
+    if(action.type==="Last_Task"){
+       lastAddedTask = action.payload
+    }
+
     return info;
 }
 
